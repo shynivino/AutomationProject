c0b48f134230d891d3f62bb87ea6494229c57f83
testingworldglobal <testingworldglobal@gmail.com>
2017-04-08 12:04:19 +0530

    Automation Project Started

---
+++ AutomationProject/src/testcases/TC_001.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test
+	public void tc_001()
+	{
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		driver.findElementById("email").sendKeys("data1");
+		driver.findElementById("pass").sendKeys("data2");
+		driver.quit();
+	}
+
+}
